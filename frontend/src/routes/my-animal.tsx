d0a34fa4a5e5ff9998c6297f6690729a75e6b6b8
forexms78 <49295076+forexms78@users.noreply.github.com>
2023-04-27 21:10:25 +0900

    My Animal Cards 페이지 작성
    에러난 부분 for문을 제대로 작성하지 않음
     for (let i = 0; i < parseInt(balanceLength, 10); i++) {
    괄호를 i < 를 작성하지 않아서 account를 제대로 불러올 수 없었음

--- frontend/src/routes/my-animal.tsx
+++ frontend/src/routes/my-animal.tsx
@@ -1,11 +1,59 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
+import { mintAnimalTokenContract } from "../contracts";
+import { Grid } from "@chakra-ui/react";
+import AnimalCard from "../components/AnimalCard";
 
 interface MyAnimalProps {
   account: string;
 }
 
-const MyAnimal: FC<MyAnimalProps> = () => {
-  return <div>MA</div>;
+const MyAnimal: FC<MyAnimalProps> = ({ account }) => {
+  const [animalCardArray, setAnimalCardArray] = useState<string[]>();
+
+  const getAnimalTokens = async () => {
+    try {
+      const balanceLength = await mintAnimalTokenContract.methods
+        .balanceOf(account)
+        .call();
+
+      const tempAnimalCardArray = [];
+
+      for (let i = 0; i < parseInt(balanceLength, 10); i++) {
+        const animalTokenId = await mintAnimalTokenContract.methods
+          .tokenOfOwnerByIndex(account, i)
+          .call();
+
+        const animalType = await mintAnimalTokenContract.methods
+          .animalTypes(animalTokenId)
+          .call();
+
+        tempAnimalCardArray.push(animalType);
+      }
+
+      setAnimalCardArray(tempAnimalCardArray);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!account) return;
+
+    getAnimalTokens();
+  }, [account]);
+
+  useEffect(() => {
+    console.log(animalCardArray);
+  }, [animalCardArray]);
+
+  return (
+    <Grid templateColumns="repeat(4,1fr)" gap={8}>
+      {animalCardArray &&
+        animalCardArray.map((v, i) => {
+          return <AnimalCard key={i} animalType={v} />;
+        })}
+    </Grid>
+  );
 };
 
 export default MyAnimal;
