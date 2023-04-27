4291db0e8f4e1e1371e5e65abee6e9c7a00319f9
forexms78 <49295076+forexms78@users.noreply.github.com>
2023-04-27 20:13:00 +0900

    mint 성공

--- frontend/src/routes/main.tsx
+++ frontend/src/routes/main.tsx
@@ -1,8 +1,65 @@
-import React, {FC} from "react";
-import {Box} from '@chakra-ui/react'
+import React, { FC, useState } from "react";
+import { Box, Button, Flex, Text, Image } from "@chakra-ui/react";
+import { mintAnimalTokenContract } from "../contracts";
+import AnimalCard from "../components/AnimalCard";
 
-const Main: FC = () => {
-    return <Box>Main</Box>
+interface MainProps {
+  account: string;
 }
 
-export default Main
+const Main: FC<MainProps> = ({ account }) => {
+  const [newAnimalType, setNewAnimalType] = useState<string>();
+
+  const onClickMint = async () => {
+    try {
+      if (!account) return;
+
+      const response = await mintAnimalTokenContract.methods
+        .mintAnimalToken()
+        .send({ from: account });
+
+      if (response.status) {
+        const balanceLength = await mintAnimalTokenContract.methods
+          .balanceOf(account)
+          .call();
+
+        const animalTokenId = await mintAnimalTokenContract.methods
+          .tokenOfOwnerByIndex(account, parseInt(balanceLength, 10) - 1)
+          .call();
+
+        const animalType = await mintAnimalTokenContract.methods
+          .animalTypes(animalTokenId)
+          .call();
+
+        setNewAnimalType(animalType);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Flex
+      w="full"
+      h="100vh"
+      justifyContent="center"
+      align="center"
+      direction="column"
+    >
+      <Box>
+        {newAnimalType ? (
+          <AnimalCard animalType={newAnimalType} />
+        ) : (
+          <Text>Let's mint AnimalCard!!</Text>
+        )}
+      </Box>
+      <Box>
+        <Button mt={4} size="sm" colorScheme="blue" onClick={onClickMint}>
+          Mint
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Main;
